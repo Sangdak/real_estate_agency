42356de4108cfe566bf878b43051d34fb30b3b8d
Sangdak <82779027+Sangdak@users.noreply.github.com>
2022-12-09 05:41:27 +1000

    review_2/4 - allowed 'flats' be blank.

--- property/models.py
+++ property/models.py
@@ -65,7 +65,7 @@ class Owner(models.Model):
     owner = models.CharField('Ф.И.О. владельца', max_length=200, db_index=True,)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True,)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', null=True, blank=True, db_index=True,)
-    flats = models.ManyToManyField(Flat, verbose_name='Квартиры в собственности', related_name='owners')
+    flats = models.ManyToManyField(Flat, verbose_name='Квартиры в собственности', related_name='owners', blank=True,)
 
     def __str__(self):
         return self.owner
